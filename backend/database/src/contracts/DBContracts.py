0d7014c45d61b0a86f165f0a0dc38b8bbdc26aa9
Liviu Tofan <liviu.tofan@isa.utm.md>
2024-09-28 20:03:29 +0300

    changed create_tables

--- backend/database/src/contracts/DBContracts.py
+++ backend/database/src/contracts/DBContracts.py
@@ -1,6 +1,5 @@
 import sqlite3
 import os
-import datetime
 
 
 class DBContracts:
@@ -8,15 +7,14 @@ class DBContracts:
         self.conn = conn
         self.log('DBContracts initialized')
         getPath = os.path.join(os.path.dirname(__file__), 'sql/get.sql')
-        insertPath = os.path.join(os.path.dirname(__file__), 'sql/insert.sql')
+        self.log(f'getPath: {getPath}')
         self.queries = {
             "get": open(getPath).read(),
         }
 
     def log(self, message):
         header = "[DATABASE][CONTRACTS]"
-        currentTime = datetime.datetime.now()
-        print(f'{header} {currentTime} {message}')
+        print(f'{header} {message}')
 
     def get(self, id: str):
         cursor = self.conn.cursor()
